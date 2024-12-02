d1d7255e494086a67d6eb2e5701756cd9242f03a
cbergeron1 <cjberger1993@outlook.com>
2024-12-01 22:51:54 -0500

    Add setup, and restructure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='served',
+    version='0.0.1',
+    description='Python Discord bot for dedicated server hosting',
+    author='Carlton Bergeron',
+    author_email='cjberger1993@outlook.com',
+    url='https://served.gg',
+    packages=find_packages(where='served'),
+)
